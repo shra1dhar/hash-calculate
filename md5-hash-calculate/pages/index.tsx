a0ef47683a1f1e688526a530d3457ac4315c8ba7
shra1dhar <shraonedhar@gmail.com>
2021-10-11 23:23:11 +0530

    Updating SEO related changes

--- md5-hash-calculate/pages/index.tsx
+++ md5-hash-calculate/pages/index.tsx
@@ -2,6 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import HomePage from '../src/components/home-page'
 
+/*
+ * {@link - https://realfavicongenerator.net/}
+ */
 const Home: NextPage = () => {
 	return (
 		<div>
@@ -11,6 +14,10 @@ const Home: NextPage = () => {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 				<meta property="og:type" content="article" />
+				<meta name="application-name" content="MD5 Hash Calculate" />
+
+				<meta name="google" content="notranslate" />
+
 				<meta
 					name="description"
 					content="MD5 online hash generator. Algorithm to generate checksum for md5 in the browser"
@@ -19,7 +26,6 @@ const Home: NextPage = () => {
 					property="og:description"
 					content="MD5 online hash generator. Algorithm to generate checksum for md5 in the browser"
 				/>
-				<meta name="robots" content="..., ..." />
 				<meta
 					name="keywords"
 					content="MD5 Hash Calculator, MD5 Checker, MD5 Converter, MD5 
@@ -27,10 +33,29 @@ Checksum Verifier, Generate MD5, Compute MD5, Calculate MD5"
 				/>
 				<meta property="og:title" content="MD5 Hash Calculator" />
 
-				<link rel="shortcut icon" href="//dpidudyah7i0b.cloudfront.net/favicon.ico" type="image/x-icon" />
-				<link rel="icon" href="//dpidudyah7i0b.cloudfront.net/favicon.ico" type="image/x-icon" />
-				<link rel="manifest" href="/manifest.json" />
-				<link rel="apple-touch-icon" href="/logo-96x96.png" />
+				<link rel="canonical" href="https://md5.hash-calculate.com" />
+
+				{/* Favicon */}
+				<link rel="apple-touch-icon" sizes="196x196" href="/md5/img/logo196.png" />
+				<link rel="apple-touch-icon" sizes="144x144" href="/md5/logo/logo144.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/md5/logo/apple-touch-icon.png" />
+				<link rel="apple-touch-icon" sizes="144x144" href="/md5/logo/logo144.png" />
+				<link rel="apple-touch-icon" sizes="72x72" href="/md5/logo/logo72.png" />
+				<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/md5/img/logo152.png" />
+
+				<link rel="icon" href="/md5/logo/icon.svg" sizes="any" type="image/svg+xml" />
+
+				<link rel="icon" type="image/png" sizes="32x32" href="/md5/logo/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/md5/logo/favicon-16x16.png" />
+
+				<link rel="manifest" href="/md5/logo/site.webmanifest" />
+				<link rel="mask-icon" href="/md5/logo/safari-pinned-tab.svg" color="#5bbad5" />
+				<link rel="shortcut icon" href="/md5/logo/favicon.ico" />
+
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="msapplication-config" content="/md5/logo/browserconfig.xml" />
+				<meta name="theme-color" content="#333333" />
+
 				<title>MD5 Online Hash Calculator</title>
 			</Head>
 
